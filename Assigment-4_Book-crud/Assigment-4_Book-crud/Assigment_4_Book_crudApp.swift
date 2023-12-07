23b2850a5c370abb694f32a4b9799e686678d6f3
Aryan Panchal <aryan.panchal.w@gmail.com>
2023-12-07 10:37:57 -0500

    Crud Ui Started in swift Ui

---
+++ Assigment-4_Book-crud/Assigment-4_Book-crud/Assigment_4_Book_crudApp.swift
@@ -0,0 +1,17 @@
+//
+//  Assigment_4_Book_crudApp.swift
+//  Assigment-4_Book-crud
+//
+//  Created by Aryan Panchal on 2023-12-07.
+//
+
+import SwiftUI
+
+@main
+struct Assigment_4_Book_crudApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
